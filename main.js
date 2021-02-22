e4ca4fad5387eb36b905776e7c12e3784a3af602
prernakakria123 <kakria99@gmail.com>
2021-02-22 16:16:00 +0530

    new code

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+require('update-electron-app')()
+const { app, BrowserWindow, ipcMain,autoUpdater,dialog } = require('electron');
 const log = require('electron-log');
 let mainWindow;
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+autoUpdater.setFeedURL({
+  "provider": "github",
+  "owner": "prerna",
+  "repo": "uengage-electron"
+});
+setInterval(() => {
+  console.log("checking..");
+  autoUpdater.checkForUpdates()
+}, 60000)
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
 
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -38,41 +60,37 @@ app.on('activate', function () {
     createWindow();
   }
 });
-autoUpdater.setFeedURL({
-  "provider": "github",
-  "owner": "prerna",
-  "repo": "uengage-electron"
-});
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-autoUpdater.on('checking-for-update', () => {
-  console.log("checking for updates");
-})
-autoUpdater.on('update-available', () => {
-  console.log("Update Available");
-  mainWindow.webContents.send('update_available');
-});
-autoUpdater.on('update-not-available', () => {
-  console.log("Update Not Available", info.version);
-  mainWindow.webContents.send('update_available');
-});
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  console.log(log_message);
-})
-autoUpdater.on('error', (err) => {
-  console.log('Error in auto-updater. ' + err);
-})
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
-});
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
-app.on('ready', function () {
-  autoUpdater.checkForUpdatesAndNotify();
-});
+
+// ipcMain.on('app_version', (event) => {
+//   event.sender.send('app_version', { version: app.getVersion() });
+// });
+// autoUpdater.on('checking-for-update', () => {
+//   console.log("checking for updates");
+// })
+// autoUpdater.on('update-available', () => {
+//   console.log("Update Available");
+//   mainWindow.webContents.send('update_available');
+// });
+// autoUpdater.on('update-not-available', () => {
+//   console.log("Update Not Available", info.version);
+//   mainWindow.webContents.send('update_available');
+// });
+// autoUpdater.on('download-progress', (progressObj) => {
+//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
+//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+//   console.log(log_message);
+// })
+// autoUpdater.on('error', (err) => {
+//   console.log('Error in auto-updater. ' + err);
+// })
+// autoUpdater.on('update-downloaded', () => {
+//   mainWindow.webContents.send('update_downloaded');
+// });
+// ipcMain.on('restart_app', () => {
+//   autoUpdater.quitAndInstall();
+// });
+// app.on('ready', function () {
+//   autoUpdater.checkForUpdatesAndNotify();
+// });
