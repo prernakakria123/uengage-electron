d0be7c705e291825bb61f98b2021917170ef3a72
prernakakria123 <kakria99@gmail.com>
2021-02-23 10:27:42 +0530

    new code

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const log = require('electron-log');
 let mainWindow;
 autoUpdater.setFeedURL({
   "provider": "github",
+  "url":"https://github.com/prernakakria123/uengage-electron.git",
   "owner": "prerna",
-  "repo": "uengage-electron",
-  "url":"https://github.com/prernakakria123/uengage-electron.git"
+  "repo": "uengage-electron"
 });
 setInterval(() => {
   console.log("checking..");
@@ -34,52 +34,52 @@ function createWindow() {
 app.on('ready', () => {
   createWindow();
 });
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  }
+// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: 'info',
+//     buttons: ['Restart', 'Later'],
+//     title: 'Application Update',
+//     message: process.platform === 'win32' ? releaseNotes : releaseName,
+//     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+//   }
 
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
+//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
+//   })
+// })
 autoUpdater.on('error', message => {
   console.error('There was a problem updating the application')
   console.error(message)
 })
 
-// app.on('window-all-closed', function () {
-//   if (process.platform !== 'darwin') {
-//     app.quit();
-//   }
-// });
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
-// app.on('activate', function () {
-//   if (mainWindow === null) {
-//     createWindow();
-//   }
-// });
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
-// autoUpdater.on('checking-for-update', () => {
-//   console.log("checking for updates");
-// })
-// autoUpdater.on('update-available', () => {
-//   console.log("Update Available");
-//   mainWindow.webContents.send('update_available');
-// });
+autoUpdater.on('checking-for-update', () => {
+  console.log("checking for updates");
+})
+autoUpdater.on('update-available', () => {
+  console.log("Update Available");
+  mainWindow.webContents.send('update_available');
+});
 
-// autoUpdater.on('error', (err) => {
-//   console.log('Error in auto-updater. ' + err);
-// })
-// autoUpdater.on('update-downloaded', () => {
-//   mainWindow.webContents.send('update_downloaded');
-// });
-// ipcMain.on('restart_app', () => {
-//   autoUpdater.quitAndInstall();
-// });
+autoUpdater.on('error', (err) => {
+  console.log('Error in auto-updater. ' + err);
+})
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
