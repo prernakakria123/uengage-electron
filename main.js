0b39416df110c262ce28650b51cd06d6388f7ffb
prernakakria123 <kakria99@gmail.com>
2021-02-23 11:42:19 +0530

    abc

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-require('update-electron-app')()
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const log = require('electron-log');
 let mainWindow;
-autoUpdater.setFeedURL({
-  "provider": "github",
-  "owner": "prerna",
-  "repo": "uengage-electron"
-});
-setInterval(() => {
-  console.log("checking..");
-  mainWindow.webContents.send('checking');
-  autoUpdater.checkForUpdates()
-}, 60000)
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -33,23 +26,6 @@ function createWindow() {
 app.on('ready', () => {
   createWindow();
 });
-// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-//   const dialogOpts = {
-//     type: 'info',
-//     buttons: ['Restart', 'Later'],
-//     title: 'Application Update',
-//     message: process.platform === 'win32' ? releaseNotes : releaseName,
-//     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-//   }
-
-//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
-//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
-//   })
-// })
-autoUpdater.on('error', message => {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
@@ -62,6 +38,7 @@ app.on('activate', function () {
     createWindow();
   }
 });
+
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
@@ -72,13 +49,30 @@ autoUpdater.on('update-available', () => {
   console.log("Update Available");
   mainWindow.webContents.send('update_available');
 });
-
+autoUpdater.on('update-not-available', () => {
+  console.log("Update Not Available", info.version);
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  console.log(log_message);
+})
 autoUpdater.on('error', (err) => {
-  console.log('Error in auto-updater. ' + err);
+  sendStatusToWindow('Error in auto-updater. ' + err);
 })
 autoUpdater.on('update-downloaded', () => {
   mainWindow.webContents.send('update_downloaded');
 });
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
+});
+autoUpdater.setFeedURL({
+  "provider": "github",
+  "owner": "prerna",
+  "repo": "uengage-electron"
+});
+app.on('ready', function () {
+  autoUpdater.checkForUpdatesAndNotify();
 });
